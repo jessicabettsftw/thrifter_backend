594fb3931b3d53be1f8d8f0ae275d268d52029c2
Jessica Betts <jessicaannbettsis@gmail.com>
2019-04-30 17:49:21 -0700

    added validation to user create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :finds
   has_many :likes
+
+  validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 16}
+  validates :email, presence: true, uniqueness: true,
+  validates :password, presence: true, length: {minimum: 6, maximum: 16}
 end
