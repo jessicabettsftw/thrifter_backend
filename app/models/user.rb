5c4057300fbd162fe838c07d0cdf2228ceee3f18
Jessica Betts <jessicaannbettsis@gmail.com>
2019-04-30 19:28:44 -0700

    edited password validation due to bad seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :likes
 
   validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 16}
-  validates :email, presence: true, uniqueness: true,
-  validates :password, presence: true, length: {minimum: 6, maximum: 16}
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: {minimum: 3, maximum: 16}
 end
