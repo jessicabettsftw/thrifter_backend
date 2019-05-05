0b06494550069a11041f974479901cab4754f46e
Jessica Betts <jessicaannbettsis@gmail.com>
2019-05-05 13:43:13 -0700

    added bcrpt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 16}
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: {minimum: 3, maximum: 16}
+
+  has_secure_password
 end
