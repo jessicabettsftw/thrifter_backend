d0ad4f409d94fb0fc59f80386b141fa4d0d35c59
Jessica Betts <jessicaannbettsis@gmail.com>
2019-05-05 17:01:02 -0700

    halfway through the jwt changes, namespace not working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  skip_before_action :authorized, only: [:create]
 
   def index
     @users = User.all
@@ -13,17 +13,13 @@ class UsersController < ApplicationController
     end
   end
 
-  def login
-    @user = User.find_by(email: params[:email], password_digest: params[:password])
-    if @user.valid?
-      render json: @user
-    end
-  end
-
   def create
     @user = User.create(user_params)
     if @user.valid?
-      render json: @user
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
     end
   end
 
