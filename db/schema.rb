b759ab04bdadcb606d6d40951b054777e4a2172a
Jessica Betts <jessicaannbettsis@gmail.com>
2019-05-07 09:37:54 -0700

    changed to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_04_25_221437) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "finds", force: :cascade do |t|
     t.integer "user_id"
     t.integer "store_id"
