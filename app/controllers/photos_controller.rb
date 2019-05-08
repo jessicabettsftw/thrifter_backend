22c2cbd329cc2a26e67dc9cd9b055cdee5c9ab30
Jessica Betts <jessicaannbettsis@gmail.com>
2019-05-07 17:53:41 -0700

    got rid of dead code

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,29 +3,22 @@ require 'base64';
 
 class PhotosController < ApplicationController
   skip_before_action :verify_authenticity_token
-  skip_before_action :authorized, only: [:upload_file]
+  # skip_before_action :authorized, only: [:upload_file]
 
   def upload_file
-    s3 = S3_BUCKET
-
-    file = '/Users/jekka/Desktop/simple.png'
-    bucket = 'serialthrifterimages'
-
-    # Get just the file name
-    name = File.basename(file)
 
     name = params[:name]
     base64 = params[:file]
     body = Base64.decode64(base64.split(',')[1])
 
     # Create the object to upload
-    obj = s3.object(name).put(
+    obj = S3_BUCKET.object(name).put(
       body: body,
       # acl: 'public-read',
       content_type: 'image/jpeg',
       content_encoding: 'base64'
     )
-    render json: {public_url: s3.object(name).presigned_url(:get)}
+    render json: {public_url: S3_BUCKET.object(name).presigned_url(:get)}
   end
 
 end
